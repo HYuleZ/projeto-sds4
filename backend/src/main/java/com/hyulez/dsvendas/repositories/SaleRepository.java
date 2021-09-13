e30b22a5c0f8b232750d4f794113d3de01cbbc68
HYuleZ <yzhenrique@gmail.com>
2021-09-12 22:10:29 -0400

    Group by search

--- backend/src/main/java/com/hyulez/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/hyulez/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,20 @@
 package com.hyulez.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.hyulez.dsvendas.dto.SaleSuccessDTO;
+import com.hyulez.dsvendas.dto.SaleSumDTO;
 import com.hyulez.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
+	@Query("SELECT new com.hyulez.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.hyulez.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
