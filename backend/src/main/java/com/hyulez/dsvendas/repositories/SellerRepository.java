702829f2899bf678ebaf386e85d4c620918541ce
HYuleZ <yzhenrique@gmail.com>
2021-09-12 21:19:45 -0400

    Layers

---
+++ backend/src/main/java/com/hyulez/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.hyulez.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hyulez.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/hyulez/dsvendas/service/SellerService.java
@@ -0,0 +1,24 @@
+package com.hyulez.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hyulez.dsvendas.dto.SellerDTO;
+import com.hyulez.dsvendas.entities.Seller;
+import com.hyulez.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
